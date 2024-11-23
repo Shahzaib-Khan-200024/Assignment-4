54c141cf31a3ce75df57f5a808eaeb78f27b42e0
Shahzaib-Khan-200024 <codewithkhan946@gmail.com>
2024-11-23 23:08:55 +0500

    Wow.

---
+++ my-app/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // Debugging ke liye React Strict Mode enable karein
+  reactStrictMode: true,
+
+  // Yahan apne project ke additional configurations add karen
+};
+
+export default nextConfig;
